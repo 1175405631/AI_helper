51498b7a50907214fe065396ffb9d083ddd96c7b
Kuo Gong <72193866+1175405631@users.noreply.github.com>
2024-09-09 22:08:01 -0400

    Second commit

--- portfolio.py
+++ portfolio.py
@@ -5,7 +5,7 @@ import uuid
 class Portfolio:
     def __init__(self, file_path="resource/my_portfolio.csv"):
         self.file_path = file_path
-        self.data = pd.read_csv(file_path)
+        self.data = pd.read_csv(file_path, error_bad_lines=False, warn_bad_lines=True)
         self.chroma_client = chromadb.PersistentClient('vectorstore')
         self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
